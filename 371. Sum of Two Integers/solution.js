6511dd6639bdd23e5a110cd43a8538bcacba3278
Chieh-Wen Yang <chuck.yang@hackathon.tw>
2016-07-19 14:04:58 +0800

    Add the solution of the problem 371.

---
+++ 371. Sum of Two Integers/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    while (b !== 0) {
+        let x = a & b;
+        a = a ^ b;
+        b = x << 1;
+    }
+    return a;
+};
+
+console.log(getSum(1, 2));
---
+++ 371. Sum of Two Integers/solution_es6.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+const getSum = (a, b) => {
+    while (b !== 0) {
+        let x = a & b;
+        a = a ^ b;
+        b = x << 1;
+    }
+    return a;
+};
+
+console.log(getSum(1, 2));
