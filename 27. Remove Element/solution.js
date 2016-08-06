90dd1dc089d440723b42fb1453da0114eb0d8485
Rukeith <fenglan.crazydance@gmail.com>
2016-08-06 13:32:22 +0800

    Add problem 27

---
+++ 27. Remove Element/solution.js
@@ -0,0 +1,36 @@
+/**
+ * 27. Remove Element
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+ *
+ * Example:
+ * Given input array nums = [3,2,2,3], val = 3
+ * Your function should return length = 2, with the first two elements of nums being 2.
+ * 
+ * Hint:
+ * Try two pointers.
+ * Did you use the property of "the order of elements can be changed"?
+ * What happens when the elements to remove are rare?
+ *
+ * URL:
+ * https://leetcode.com/problems/remove-element/
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+  var i = 0;
+
+  for (var j = 0; j < nums.length; j++) {
+      if (nums[j] !== val) {
+          nums[i] = nums[j];
+          i++;
+      }
+  }
+
+  return i;
+};
