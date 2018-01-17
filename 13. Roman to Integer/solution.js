cf1b14febca4e91d3d4b09358941b064eee58f04
Rukeith <fenglan.crazydance@gmail.com>
2018-01-17 21:49:19 +0800

    Add 13 and 14 problems

---
+++ 13. Roman to Integer/solution.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {number}
+ * 
+ * 13. Roman to Integer
+ * Given a roman numeral, convert it to an integer.
+ * Input is guaranteed to be within the range from 1 to 3999.
+ * 
+ * URL:
+ * https://leetcode.com/problems/roman-to-integer/description/
+ */
+var romanToInt = function(s) {
+  const hash = {
+    M: 1000,
+    D: 500,
+    C: 100,
+    L: 50,
+    X: 10,
+    V: 5,
+    I: 1
+  };
+
+  let sum = 0;
+  let lastValue = null;
+  for(let n = s.length - 1; n > -1; n--) {
+    const char = s.charAt(n);
+    const charVal = hash[char];
+    
+    if (lastValue && lastValue > charVal) {
+      sum -= charVal;
+    } else {
+      sum += charVal;
+      lastValue = charVal;
+    }
+  }
+
+  return sum;
+};
---
+++ 14. Longest Common Prefix/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ * 
+ * 14. Longest Common Prefix
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * 
+ * URL:
+ * https://leetcode.com/problems/longest-common-prefix/description/
+ */
+var longestCommonPrefix = function(strs) {
+  const len = strs.length;
+  if (len == 0) return "";
+  let prefix = strs[0];
+
+  for (let i = 1; i < len; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+    }
+  }
+  return prefix;
+};
