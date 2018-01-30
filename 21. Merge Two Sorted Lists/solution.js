ea189bf958eb194ab405300fffd83b93ebebac50
Rukeith <fenglan.crazydance@gmail.com>
2018-01-30 14:21:15 +0800

    Add problem 21 and 26

---
+++ 21. Merge Two Sorted Lists/solution.js
@@ -0,0 +1,32 @@
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ * 
+ * 21. Merge Two Sorted Lists
+ * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+ * 
+ * Example:
+ *  Input: 1->2->4, 1->3->4
+ *  Output: 1->1->2->3->4->4
+ * 
+ * URL:
+ * https://leetcode.com/problems/merge-two-sorted-lists/description/
+ * 
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+var mergeTwoLists = function(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l2.next, l1);
+    return l2;
+  }
+};
---
+++ 26. Remove Duplicates from Sorted Array/solution.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * 
+ * 26. Remove Duplicates from Sorted Array
+ * Given a sorted array, remove the duplicates in-place such that each element appear only once and return the new length.
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * 
+ * Example:
+ * Given nums = [1,1,2],
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ * And the origin nums should resort elements like [1,2,1]. It doesn't matter what you leave beyond the new length.
+ * 
+ * URL:
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+ * 
+ */
+var removeDuplicates = function(nums) {
+  if (nums.length < 2) return nums;
+  let i = 0;  // Store the last number position
+  for (let index = 1; index < nums.length; index++) {
+    if (nums[index] != nums[i]) {
+      i++;
+      nums[i] = nums[index];
+    }
+  }
+  return i + 1;
+};
