b0810bb807e5f98b25521daafc9072956e35e977
Rukeith <fenglan.crazydance@gmail.com>
2018-01-31 15:20:28 +0800

    修正縮排

--- 1. Two Sum/solution.js
+++ 1. Two Sum/solution.js
@@ -18,8 +18,8 @@
 var twoSum = function(nums, target) {
   const index = {}
   for (let i = 0; i < nums.length; i++) {
-      const matchNum = target - nums[i];
-      if (typeof index[matchNum] === 'number') return [index[matchNum], i]
-      index[nums[i]] = i;
+    const matchNum = target - nums[i];
+    if (typeof index[matchNum] === 'number') return [index[matchNum], i]
+    index[nums[i]] = i;
   }
 };
