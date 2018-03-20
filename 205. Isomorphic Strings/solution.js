304999e658ff4a74581ea58ac9f9e9b1604d32da
Rukeith <fenglan.crazydance@gmail.com>
2018-03-20 09:13:31 +0800

    Add 205 and 206

---
+++ 205. Isomorphic Strings/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+  let m1 = {}, m2 = {}, n = s.length;
+  for (let i = 0; i < n; ++i) {
+      if (m1[s[i]] !== m2[t[i]]) return false;
+      m1[s[i]] = i + 1;
+      m2[t[i]] = i + 1;
+  }
+  return true;
+};
---
+++ 206. Reverse Linked List/README.md
@@ -0,0 +1,5 @@
+# [206. Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/description)
+Reverse a singly linked list.
+
+**Hint:**
+A linked list can be reversed either iteratively or recursively. Could you implement both?
