ee447737593cb1551413e9fe523a53e995085b05
Rukeith <fenglan.crazydance@gmail.com>
2018-03-26 09:18:56 +0800

    Add 234 and 235

---
+++ 234. Palindrome Linked List/solution.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  const arrNode = [];
+  let currNode = head;
+  while(currNode) {
+    arrNode.push(currNode.val);
+    currNode = currNode.next;
+  }
+
+  for(let i = 0; i < arrNode.length / 2; i++) {
+    if (arrNode[i] !== arrNode[arrNode.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
---
+++ 235. Lowest Common Ancestor of a Binary Search Tree/solution.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  if (!root) return root;
+  if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  } else if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  } else {
+    return root;
+  }
+};
