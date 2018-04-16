9181e2fee47df79662894668546c1fc421f93bf0
Rukeith <fenglan.crazydance@gmail.com>
2018-04-16 09:37:35 +0800

    ADD 401 and 404

---
+++ 401. Binary Watch/solution.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} num
+ * @return {string[]}
+ */
+var readBinaryWatch = function(num) {
+  const getBitAmount = (n) => {
+    let ans = 0;
+    while (n > 0) {
+      ans += (n % 2);
+      n = Math.floor(n / 2);
+    }
+    return ans;
+  };
+
+  const bitList = {};
+  for (let b = 0; b < 60; b++) {
+    const a = getBitAmount(b);
+    if (!bitList[a]) {
+      bitList[a] = [];
+    }
+    bitList[a].push(b);
+  }
+
+  const ans = [];
+  
+  for (let i = 0; i <= num; i++) {
+    if (!(bitList[i] && bitList[num - i])) continue;
+    bitList[i].map((h) => {
+      if (h < 12) {
+        const hStr = `${h}:`;
+        bitList[num - i].map((m) => {
+          if (m < 60) {
+            ans.push(hStr + (m < 10 ? '0' : '') + m);
+          }
+        });
+      }
+    });
+  }
+
+  return ans;
+};
