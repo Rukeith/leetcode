90d84d5b80daac3c82f01d67948db1541a5df697
Rukeith <fenglan.crazydance@gmail.com>
2018-04-26 08:48:04 +0800

    Update 404

--- 404. Sum of Left Leaves/solution.js
+++ 404. Sum of Left Leaves/solution.js
@@ -10,10 +10,10 @@
  * @return {number}
  */
 var sumOfLeftLeaves = function(root) {
-  if(!root) return 0;
-  if(root.left && !root.left.left && !root.left.right) {
-    return root.left.val + sumOfLeftLeaves(root.right);
-  } else {
-    return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+  let res = 0;
+  if (root) {
+    if (root.left && !root.left.left && !root.left.right) res += root.left.val;
+    return res + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
   }
+  return res;
 };
